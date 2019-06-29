192d2c4dc5123ff0df6674a4612dca50d9011440
David Valaczaki <dmv18@ic.ac.uk>
2019-06-30 01:02:07 +0200

    fetching, rendering of JSON implemented

--- src/App.js
+++ src/App.js
@@ -2,25 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: []};
+  }
+
+  componentDidMount() {
+    fetch(url)
+        .then(res => res.json())
+        .then(json => this.setState({data: json}));
+  }
+
+  render() {
+    return (
+        <div>
+          <ul className="unstyled">
+            {this.state.data.map(elem => <ListItem key={elem.id} userId={elem.userId} title={elem.title} body={elem.body}/>)}
+          </ul>
+        </div>
+    )
+  }
+}
+
+function ListItem(props) {
+  return <li>
+            <div>
+              <h1>User {props.userId}</h1>
+            </div>
+            <div>
+              <h2>{props.title}</h2>
+            </div>
+            <div>
+              {props.body}
+            </div>
+            <hr/>
+          </li>;
 }
 
 export default App;
